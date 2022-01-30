72eedbfc9bbd0c990b2daf45ba9aa0827957bcd6
wasp <wasp@rocke.tech>
2022-01-30 21:52:54 +0200

    add text files

--- src/main/java/com/wasp/io/FileAnalyzer.java
+++ src/main/java/com/wasp/io/FileAnalyzer.java
@@ -1,6 +1,7 @@
 package com.wasp.io;
 
 public class FileAnalyzer {
+    String sentenceRegex = "^\\s+[A-Za-z,;'\"\\s]+[.?!]$";
 
     public static void main(String[] args) {
         String fileName = args[0];
