3fa506e80d5eebbfa28bae698f81d591e7ab5568
wasp <wasp@rocke.tech>
2022-02-02 02:23:37 +0200

    implement FileManager

--- src/main/java/com/wasp/io/file/FileManager.java
+++ src/main/java/com/wasp/io/file/FileManager.java
@@ -1,2 +1,137 @@
-package com.wasp.io.file;public class FileManager {
+package com.wasp.io.file;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class FileManager {
+
+    /**
+     * Method that counts files in a given directory and all subdirectories
+     *
+     * @param path path to directory
+     * @return the total number of files found
+     */
+    public static int countFiles(String path) {
+        File source = new File(path);
+        checkSourceExists(source);
+        if (source.isFile()) {
+            return 1;
+        } else {
+            int count = 0;
+            List<File> files = getSubFiles(source);
+            for (File file : files) {
+                if (file.isFile()) {
+                    count++;
+                }
+            }
+            return count;
+        }
+    }
+
+    /**
+     * Method that counts underlying directories in a given directory and all subdirectories
+     *
+     * @param path path to directory
+     * @return the total number of directories found
+     */
+    public static int countDirs(String path) {
+        File source = new File(path);
+        checkSourceExists(source);
+        int count = 0;
+        List<File> files = getSubFiles(source);
+        for (File file : files) {
+            if (file.isFile()) count++;
+        }
+        return count;
+    }
+
+
+    /**
+     * Method that copies files and directories.
+     *
+     * @param from source path
+     * @param to   destination path
+     */
+    public static void copy(String from, String to) {
+        File source = new File(from);
+        File destination = new File(to);
+        checkSourceExists(source);
+        if (destination.exists()) {
+            throw re("File already exists");
+        }
+        if (source.isDirectory()) {
+            createDirectory(source, destination);
+        } else if (source.isFile()) {
+            createFile(destination);
+        }
+    }
+
+    /**
+     * Method that moves files and directories.
+     *
+     * @param from source path
+     * @param to   destination path
+     */
+    public static void move(String from, String to) {
+        copy(from, to);
+        File source = new File(from);
+        source.delete();
+    }
+
+    static boolean createDirectory(File source, File destination) {
+        boolean result = false;
+        List<File> subFiles = getSubFiles(source);
+        if (subFiles.isEmpty()) {
+            result = destination.mkdir();
+        } else {
+            for (File file : subFiles) {
+                if (file.isDirectory()) {
+                    result = file.mkdirs();
+                } else if (file.isFile()) {
+                    result = createFile(file);
+                }
+            }
+        }
+        return result;
+    }
+
+    static boolean createFile(File destination) {
+        File parent = new File(destination.getParent());
+        boolean dirsCreated = parent.mkdirs();
+        boolean fileCreated;
+        try {
+            fileCreated = destination.createNewFile();
+        } catch (IOException e) {
+            throw new RuntimeException(e.getMessage(), e);
+        }
+        return dirsCreated && fileCreated;
+    }
+
+    static List<File> getSubFiles(File source) {
+        if (!source.isDirectory()) {
+            throw re("Source should be a directory: " + source.getPath());
+        }
+        List<File> result = new ArrayList<>();
+        List<File> deepFiles = Arrays.asList(source.listFiles());
+        result.addAll(deepFiles);
+        for (File file : deepFiles) {
+            if (file.isDirectory()) {
+                result.addAll(getSubFiles(file));
+            }
+        }
+        return result;
+    }
+
+    private static void checkSourceExists(File source) {
+        if (!source.exists()) {
+            throw re("Nothing exists by this path: " + source.getPath());
+        }
+    }
+
+    private static RuntimeException re(String msg) {
+        return new RuntimeException(msg);
+    }
 }
--- src/test/java/com/wasp/io/file/FileManagerTest.java
+++ src/test/java/com/wasp/io/file/FileManagerTest.java
@@ -1,4 +1,96 @@
+package com.wasp.io.file;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+
+import java.io.File;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
-class FileManagerTest {
-  
+
+class FileManagerTests {
+    private static final String DS_STORE = ".DS_Store";
+    private static final String RESOURCES = "src/test/resources";
+    private static final String TEST_DIR_ROOT = RESOURCES + "/testDir";
+    private static final String DEEPEST_DIR_SRC = TEST_DIR_ROOT + "/1/11";
+    private static final String DEEPEST_FILE_SRC = TEST_DIR_ROOT + "/1/11/deepest.txt";
+    private static final String DEEPEST_DIR_DEST = TEST_DIR_ROOT + "/2/21";
+    private static final String DEEPEST_FILE_DEST = TEST_DIR_ROOT + "/2/21/deepest.txt";
+
+    @BeforeEach
+    void deleteDS_Store() {
+        List<File> files = FileManager.getSubFiles(new File(TEST_DIR_ROOT));
+        files.stream().filter(file -> DS_STORE.equals(file.getName())).forEach(File::delete);
+    }
+
+    @AfterEach
+    void cleanup() {
+        File source = new File(DEEPEST_FILE_SRC);
+        File destination = new File(DEEPEST_FILE_DEST);
+        if (destination.exists()) {
+            destination.delete();
+            new File(destination.getParent()).delete();
+        }
+        if (!source.exists()) {
+            FileManager.createFile(source);
+        }
+    }
+
+    @Test
+    void countFiles() {
+        assertEquals(3, FileManager.countFiles(TEST_DIR_ROOT));
+    }
+
+    @Test
+    void countOneFile() {
+        assertEquals(1, FileManager.countFiles(DEEPEST_FILE_SRC));
+    }
+
+    @Test
+    void countDirs() {
+        assertEquals(3, FileManager.countDirs(TEST_DIR_ROOT));
+    }
+
+    @Test
+    void copyFile() {
+        File source = new File(DEEPEST_FILE_SRC);
+        File destination = new File(DEEPEST_FILE_DEST);
+        assertTrue(source.exists());
+        assertTrue(source.isFile());
+        assertFalse(destination.exists());
+
+        FileManager.copy(DEEPEST_FILE_SRC, DEEPEST_FILE_DEST);
+        assertTrue(source.exists());
+        assertTrue(destination.exists());
+    }
+
+    @Test
+    void moveFile() {
+        File source = new File(DEEPEST_FILE_SRC);
+        File destination = new File(DEEPEST_FILE_DEST);
+
+        assertTrue(source.exists());
+        assertTrue(source.isFile());
+        assertFalse(destination.exists());
+
+        FileManager.move(DEEPEST_FILE_SRC, DEEPEST_FILE_DEST);
+        assertFalse(source.exists());
+        assertTrue(destination.exists());
+    }
+
+    @Test //doesn't work
+    void moveDirectory() {
+        File source = new File(DEEPEST_DIR_SRC);
+        File destination = new File(DEEPEST_DIR_DEST);
+
+        assertTrue(source.exists());
+        assertTrue(source.isDirectory());
+        assertFalse(destination.exists());
+
+        FileManager.move(DEEPEST_DIR_SRC, DEEPEST_DIR_DEST);
+        assertFalse(source.exists());
+        assertTrue(destination.exists());
+    }
 }
